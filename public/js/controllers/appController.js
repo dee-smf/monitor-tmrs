26fe16ddb84c2fea6b7543871bafc217ac5d4745
Jeferson Quadros <pianeiro@gmail.com>
2026-06-30 21:15:50 -0300

    refactor: add appController module

---
+++ public/js/controllers/appController.js
@@ -0,0 +1,9 @@
+export const handleDataLoaded = (data, { processData, formatMonthYear, setupYearSelector, updateView, state }) => {
+    state.rawData = data;
+    const result = processData(state.rawData, formatMonthYear);
+    state.processedData = result.processedData;
+    state.availableYears = result.availableYears;
+    
+    setupYearSelector(state.availableYears);
+    updateView();
+};
