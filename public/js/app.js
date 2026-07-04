49ee9b872c13c2e266beada586b75343744185a8
Jeferson Quadros <pianeiro@gmail.com>
2026-07-04 17:28:38 -0300

    refact(main): make loadTimeSeries return data, store in variable, use type=module for top-level await

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,8 @@
-console.log('Monitor TMRS frontend is working.');
+async function loadTimeSeries(jsonPath) {
+  const response = await fetch(jsonPath);
+  const data = await response.json();
+  return data;
+}
+
+const timeSeriesData = await loadTimeSeries('data/timeSeries.json');
+console.log(timeSeriesData);
