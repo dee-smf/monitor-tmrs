60d3bf7c4f545efd5c92f0258488861c4661c873
Jeferson Quadros <pianeiro@gmail.com>
2026-07-06 16:27:13 -0300

    refactor(main): rename TimeSeriesRepository to TimeSeriesRepositoryInterface and move to application layer

--- public/js/application/TimeSeriesRepositoryInterface.js
+++ public/js/application/TimeSeriesRepositoryInterface.js
@@ -1,4 +1,4 @@
-export class TimeSeriesRepository {
+export class TimeSeriesRepositoryInterface {
   constructor(path) {
     this.path = path;
   }
--- public/js/infrastructure/JsonTimeSeriesRepository.js
+++ public/js/infrastructure/JsonTimeSeriesRepository.js
@@ -1,7 +1,7 @@
-import { TimeSeriesRepository } from '../domain/TimeSeriesRepository.js';
+import { TimeSeriesRepositoryInterface } from '../application/TimeSeriesRepositoryInterface.js';
 import { TimeSeries } from '../domain/TimeSeries.js';
 
-export class JsonTimeSeriesRepository extends TimeSeriesRepository {
+export class JsonTimeSeriesRepository extends TimeSeriesRepositoryInterface {
   async load() {
     const response = await fetch(this.path);
     const raw = await response.json();
